452fe3cffab6eae82b3f4d769da7c4622c28ec27
Melhoo10 <144268481+Melhoo10@users.noreply.github.com>
2025-04-02 10:34:01 -0400

    M

--- Asteroids/Assets/Player.cs
+++ Asteroids/Assets/Player.cs
@@ -2,24 +2,48 @@ using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class Player : MonoBehaviour
 {
+    //Player Input 
     private Vector2 input_vec = Vector2.zero;
+
+    //Rotation 
+    private float rotate_speed = 200f; //In degrees. 
+
+    //componets
+    private Rigidbody2D rb;
+
+    //Moving 
+    private float max_speed = 5.5f; 
+   
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+       
+ 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Debug.Log(input_vec);
-    }
 
+        //If input player is holding down the A or D Key...
+        if (input_vec.x != 0)
+        {
+            //Rotate the player
+            rb.MoveRotation(rb.rotation + (-Mathf.Sign(input_vec.x) * rotate_speed * Time.fixedDeltaTime));
+
+        }
+        //Move the player up. 
+        rb.MovePosition(rb.position + (Vector2.up * Time.fixedDeltaTime));
+        }
+         
     public void CaptureMoveInput(InputAction.CallbackContext context)
     {
         input_vec = context.ReadValue<Vector2>(); 
     }
+    
 }
