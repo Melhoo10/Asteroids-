e6311df9bdaf6c418c47c4a99993533cd9dea88a
Melhoo10 <144268481+Melhoo10@users.noreply.github.com>
2025-04-21 10:34:29 -0400

    u

--- Asteroids/Assets/Player.cs
+++ Asteroids/Assets/Player.cs
@@ -23,7 +23,13 @@ public class Player : MonoBehaviour
 
     private Vector2 velocity = Vector2.zero;
 
-    private float friction = 1f; 
+    private float friction = 1f;
+
+    //Screen Wrapping 
+    public Camera cam;
+    private Vector2 cam_bottom_left = Vector2.zero;
+    private Vector2 cam_top_right = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +41,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        //Get camera vectors in  screen space. (Pixels)
+        cam_top_right = new Vector2(cam.scaledPixelWidth, cam.scaledPixelHeight);
+
+        //Conver them to world spacer. 
+        cam_bottom_left = cam.ScreenToWorldPoint(cam_bottom_left);
+        cam_top_right = cam.ScreenToWorldPoint(cam_top_right);
+
+
         //If input player is holding down the A or D Key...
         if (input_vec.x != 0)
         {
@@ -43,28 +58,34 @@ public class Player : MonoBehaviour
 
         }
         //if player is ho;dong down the w key 
-        if (input_vec.y > 0)
+        if (input_vec.y > 0) ;
+        else //if the player is not processing the w key...
         {
-            
+            //Decelerate.
+            velocity = Vector3.MoveTowards(velocity, Vector3.zero, friction * Time.deltaTime);
+        }
+        {
+
 
             //Add acceleration to our velocity.
             velocity += new Vector2(transform.up.x, transform.up.y) * acceleration * Time.fixedDeltaTime;
 
             //limit the velocity. 
-            velocity = Vector2.ClampMagnitude(velocity, max_speed); 
+            velocity = Vector2.ClampMagnitude(velocity, max_speed);
 
             //Move the player. 
             rb.MovePosition(rb.position + (velocity * Time.fixedDeltaTime));
 
-            
+
 
         }
-        else //If the [layer is not pressing the W key... 
+   
+        
        
         //Move the player up. 
         rb.MovePosition(rb.position + (Vector2.up * Time.fixedDeltaTime));
     }
-    
+
     public void CaptureMoveInput(InputAction.CallbackContext context)
     {
         input_vec = context.ReadValue<Vector2>(); 
